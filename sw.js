b93eb7b55b29a01606a603f4e8b2d1cbe2ff675f
victoryaoife <victoryaoife@gmail.com>
2026-03-15 18:12:39 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'xingyue-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
