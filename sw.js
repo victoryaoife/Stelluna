6eb481ed2c34740a7dae38f68e5575edef7f7be8
victoryaoife <victoryaoife@gmail.com>
2026-05-05 21:03:58 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// 自毁版 - 清除所有缓存，恢复正常
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
@@ -7,18 +6,15 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.matchAll())
-    .then(cls => {
-      cls.forEach(c => c.navigate(c.url));
-    })
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
 
-// Push 通知保留
 self.addEventListener('push', event => {
   const data = event.data ? event.data.json() : {};
   event.waitUntil(self.registration.showNotification(
